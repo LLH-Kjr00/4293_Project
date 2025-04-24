06662dc62bb71eba9d277acad2c1f466a91e56ae
Kjr00 <107486600+LLH-Kjr00@users.noreply.github.com>
2025-04-25 00:52:56 +0800

    Update basic_benchmark.sh

--- content/basic_benchmark.sh
+++ content/basic_benchmark.sh
@@ -7,8 +7,7 @@ sudo apt-get install sysbench  # Debian/Ubuntu
 sysbench cpu --cpu-max-prime=20000 run
 
 # Memory Test (Throughput)
-
- memory --memory-block-size=1K --memory-total-size=100G run
+sysbench memory --memory-block-size=1K --memory-total-size=100G run
 
 # File I/O Test (Sequential & Random)
 sysbench fileio --threads=16 --file-total-size=3G --file-test-mode=rndrw prepare
